a2fc8f0da5eeef1024b48caccd070bab83f35a93
jwhitis <jack.whitis@gmail.com>
2013-06-11 19:16:28 -0500

    New Rails project with PostgreSQL

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MixNotes::Application.config.secret_token = '6d9ccb204150e498959188998f3fddb6fbcf0555e2272608fc248c10cdae3917ef81731b70ffe37c99137933b25d253e19e828cc8b962ee46327ab7f77fd4940'
