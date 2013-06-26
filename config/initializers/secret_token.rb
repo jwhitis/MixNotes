20bf84dd2ade334b8c5dc96ef7f54588944862e6
jwhitis <jack.whitis@gmail.com>
2013-06-26 10:52:56 -0500

    Create secret_token environment variable

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,8 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-MixNotes::Application.config.secret_token = '6d9ccb204150e498959188998f3fddb6fbcf0555e2272608fc248c10cdae3917ef81731b70ffe37c99137933b25d253e19e828cc8b962ee46327ab7f77fd4940'
+if Rails.env.test? || Rails.env.development?
+  MixNotes::Application.config.secret_token = '6d9ccb204150e498959188998f3fddb6fbcf0555e2272608fc248c10cdae3917ef81731b70ffe37c99137933b25d253e19e828cc8b962ee46327ab7f77fd4940'
+else
+  MixNotes::Application.config.secret_token = ENV["SECRET_TOKEN"]
+end
